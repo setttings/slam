77d4820ba6ce89596fc7c8f60667a94cabf92adf
setttings <aeng@andrew.cmu.edu>
2017-05-12 17:11:22 -0400

    Computing baseline times
    Uses pcl kd-tree library

---
+++ build/CMakeFiles/loam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loam.dir/loam.cpp.o"
+  "loam.pdb"
+  "loam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
